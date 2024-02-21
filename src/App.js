a5513cdb1f23ecdbc7549ec63bff45f239bc7200
Sarthak Ekhande <sarthakekhande2017@gmail.com>
2024-02-21 22:35:15 +0530

    Signin/signup && login/header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1 className='font-bold p-5 m-4 border border-black shadow-lg bg-orange-600 items-center'>Heooo sartjaskl</h1>
     </div>
   );
 }
